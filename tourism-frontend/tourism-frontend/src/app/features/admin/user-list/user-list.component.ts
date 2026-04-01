01183624485ea310a7998b573cd186ec3993b8cd
Vukasin <vukasin.designer@gmail.com>
2026-04-01 15:45:04 +0200

    admin can block user accounts

--- tourism-frontend/tourism-frontend/src/app/features/admin/user-list/user-list.component.ts
+++ tourism-frontend/tourism-frontend/src/app/features/admin/user-list/user-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AdminService, UserDto } from '../../../core/services/admin.service';
+import { TokenService } from '../../../core/services/token.service';
 
 @Component({
   selector: 'app-user-list',
@@ -10,17 +10,51 @@ import { AdminService, UserDto } from '../../../core/services/admin.service';
 })
 export class UserListComponent implements OnInit {
 
-  users$!: Observable<UserDto[]>;
+  users: UserDto[] = [];
+  loading = true;
   error = '';
+  currentUserId: number;
 
-  constructor(private adminService: AdminService) {}
+  constructor(
+    private adminService: AdminService,
+    private tokenService: TokenService,
+    private cdr: ChangeDetectorRef
+  ) {
+    this.currentUserId = this.tokenService.getUser()?.id ?? 0;
+  }
 
   ngOnInit(): void {
-    this.users$ = this.adminService.getAllUsers().pipe(
-      catchError(err => {
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.loading = true;
+    this.adminService.getAllUsers().subscribe({
+      next: (users) => {
+        this.users = users;
+        this.loading = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
         this.error = err?.error?.error ?? 'Failed to load users.';
-        return of([]);
-      })
-    );
+        this.loading = false;
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  toggleBlock(user: UserDto): void {
+    this.adminService.toggleBlock(user.id).subscribe({
+      next: (updated) => {
+        const idx = this.users.findIndex(u => u.id === updated.id);
+        if (idx !== -1) {
+          this.users[idx] = updated;
+        }
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        alert(err?.error?.error ?? 'Failed to update user status.');
+      }
+    });
   }
 }
