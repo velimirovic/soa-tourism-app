ac2904b57d32878d047a14907f2be973c6a52aa7
Vukasin <vukasin.designer@gmail.com>
2026-05-20 19:49:06 +0200

    feat: Shopping cart

--- tourism-frontend/tourism-frontend/src/app/app-module.ts
+++ tourism-frontend/tourism-frontend/src/app/app-module.ts
@@ -35,6 +35,8 @@ import { KeyPointsComponent }         from './features/tours/key-points/key-poin
 import { TourPublicListComponent }    from './features/tours/tour-public-list/tour-public-list.component';
 import { TourDetailComponent }        from './features/tours/tour-detail/tour-detail.component';
 import { PositionSimulatorComponent } from './features/position-simulator/position-simulator.component';
+import { ShoppingCartComponent }      from './features/tours/shopping-cart/shopping-cart.component';
+import { MyPurchasesComponent }       from './features/tours/my-purchases/my-purchases.component';
 
 @NgModule({
   declarations: [
@@ -55,7 +57,9 @@ import { PositionSimulatorComponent } from './features/position-simulator/positi
     KeyPointsComponent,
     TourPublicListComponent,
     TourDetailComponent,
-    PositionSimulatorComponent
+    PositionSimulatorComponent,
+    ShoppingCartComponent,
+    MyPurchasesComponent
   ],
   imports: [
     BrowserModule,
